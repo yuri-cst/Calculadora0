ca00bae105f8e6abfa67ee54045e236bd11a4585
Yuri da Costa <yuri.santoa@duque.g12.br>
2025-04-14 19:52:42 -0300

    Adicionando parte de subtração e adição

---
+++ CalculadoraJava/src/main/java/com/mycompany/calculadorajava/CalculadoraJava.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorajava;
+
+/**
+ *
+ * @author 08240
+ */
+public class Main {
+    public static void main(String[] args) {
+        new CalculadoraGUI();
+    }
+}
